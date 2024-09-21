7e86081cc75b7b1a3df5f4a61ef4bae6003a0222
boa <17629179+boafur@users.noreply.github.com>
2024-09-21 03:48:42 -0700

    refactor logic

--- main.go
+++ main.go
@@ -1,41 +1,34 @@
 package main
 
 type DataTypeStruct struct {
-	Quarters     float64 `csv:"quarters"`
-	Dimes        float64 `csv:"dimes"`
-	Nickels      float64 `csv:"nickels"`
-	Pennies      float64 `csv:"pennies"`
-	ChangeNeeded float64 `csv:"changeneeded"`
-	AssertIs     bool    `csv:"assertisenoughchange"`
-}
-
-type DataMap struct {
+	Quarters       float64 `csv:"quarters"`
+	Dimes          float64 `csv:"dimes"`
+	Nickels        float64 `csv:"nickels"`
+	Pennies        float64 `csv:"pennies"`
+	ChangeNeeded   float64 `csv:"changeneeded"`
+	AssertIs       bool    `csv:"assertisenoughchange"`
 	TotalMoney     float64
-	ChangeNeeded   float64
 	IsEnoughChange bool
 }
 
-func calcData(data DataTypeStruct) DataMap {
-	mapData := DataMap{}
-	mapData.ChangeNeeded = data.ChangeNeeded
-	mapData.TotalMoney += 0.25 * data.Quarters
-	mapData.TotalMoney += 0.10 * data.Dimes
-	mapData.TotalMoney += 0.05 * data.Nickels
-	mapData.TotalMoney += 0.01 * data.Pennies
-	mapData.IsEnoughChange = mapData.TotalMoney >= mapData.ChangeNeeded
+func (d *DataTypeStruct) calcData() DataTypeStruct {
+	d.TotalMoney += 0.25 * d.Quarters
+	d.TotalMoney += 0.10 * d.Dimes
+	d.TotalMoney += 0.05 * d.Nickels
+	d.TotalMoney += 0.01 * d.Pennies
+	d.IsEnoughChange = d.TotalMoney >= d.ChangeNeeded
 
-	return mapData
+	return *d
 }
 
 func main() {
 	dataToTest, log := SetupTestData()
 
 	for i, v := range dataToTest {
-		returned := calcData(v.Data)
-		if v.ToAssert != returned.IsEnoughChange {
-			log.Error("", "index", i, "changeGiven", returned.TotalMoney, "changeNeeded", returned.ChangeNeeded, "isEnoughChange", returned.IsEnoughChange, "assertIsEnoughChange", v.ToAssert)
+		if v.ToAssert != v.Data.calcData().IsEnoughChange {
+			log.Error("", "index", i, "changeGiven", v.Data.TotalMoney, "changeNeeded", v.Data.ChangeNeeded, "isEnoughChange", v.Data.IsEnoughChange, "assertIsEnoughChange", v.ToAssert)
 		} else {
-			log.Info("", "index", i, "changeGiven", returned.TotalMoney, "changeNeeded", returned.ChangeNeeded, "isEnoughChange", returned.IsEnoughChange, "assertIsEnoughChange", v.ToAssert)
+			log.Info("", "index", i, "changeGiven", v.Data.TotalMoney, "changeNeeded", v.Data.ChangeNeeded, "isEnoughChange", v.Data.IsEnoughChange, "assertIsEnoughChange", v.ToAssert)
 		}
 	}
 }
